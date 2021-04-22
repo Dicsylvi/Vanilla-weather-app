e7445ce2006061826a0d3a0f7bbefa14f8ffdc3a
Dicsylvi <dicsylvi@gmail.com>
2021-04-22 15:13:40 -0400

    API Icon integration

--- src/app.js
+++ src/app.js
@@ -1,14 +1,13 @@
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
-  let minutes = date.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-
-if (hours <10) {
+  if (hours <10) {
   hours = `0${hours}`;
-}
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+  minutes = `0${minutes}`;
+  }
 
   let days =  [ 
    "Sunday",
@@ -18,8 +17,7 @@ if (hours <10) {
    "Thursday",
    "Friday", 
    "Saturday"
- ];
-  
+   ];
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
@@ -31,18 +29,25 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon") ;
+  
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute (
+    "src",
+     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+
+     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 
 
 let apiKey = "b0760482434c16aa42a44f516f0ce8a8";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Bear&appid=${apiKey}&units=metric`;
+let city = "New Castle";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
