b13a136fe49c654ae49bd66e7be4cb1332b402aa
Dicsylvi <dicsylvi@gmail.com>
2021-04-23 11:43:27 -0400

    Added search engine form

--- src/app.js
+++ src/app.js
@@ -44,10 +44,19 @@ function displayTemperature(response) {
      iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-
-
+function search(city) {
 let apiKey = "b0760482434c16aa42a44f516f0ce8a8";
-let city = "New Castle";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
 axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+event.preventDefault();
+let cityInputElement = document.querySelector("#city-input");
+search(cityInputElement.value);
+}
+
+search("Bear");
+
+let form = document.querySelector("#search-form")
+form.addEventListener("submit", handleSubmit );
